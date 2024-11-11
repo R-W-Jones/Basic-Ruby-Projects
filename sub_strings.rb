8067f711452c2032a0401ead555c30801174e766
R-W-Jones <75536424+R-W-Jones@users.noreply.github.com>
2024-11-11 12:35:53 +0000

    TOP sub strings assignment

---
+++ sub_strings.rb
@@ -0,0 +1,18 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(input, dictionary)
+  input = input.downcase
+  matching_hash = Hash.new(0)
+
+  dictionary.each do |word|
+    input.scan(word).each do
+      matching_hash[word] += 1
+    end
+  end
+
+  matching_hash
+end
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+
